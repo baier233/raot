1196e7501f17c9b3e80296288480df4be4b4087a
Peyriat <Peyriat@SHEYA>
2025-07-07 21:07:33 +0800

    [*] Fix MouseDrawCursor

--- raot/src/base/GitInfo.h
+++ raot/src/base/GitInfo.h
@@ -1,3 +1,3 @@
 #pragma once
-#define GIT_COMMIT_HASH "081ce6f"
-#define GIT_COMMIT_TIME "Mon Jul 7 14:27:52 2025 +0800"
+#define GIT_COMMIT_HASH "fdf9b4e"
+#define GIT_COMMIT_TIME "Mon Jul 7 20:30:19 2025 +0800"
--- raot/src/base/render/gui/gui.cpp
+++ raot/src/base/render/gui/gui.cpp
@@ -8,65 +8,72 @@
 #include <format>
 #include <base/render/d3dhook.h>
 #include <base/sdk/sdk.h>
+
 extern auto ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) -> LRESULT;
+
 static void render_gui_internal() {
-	ImGui::SetNextWindowSize(ImVec2(300, 100), ImGuiCond_FirstUseEver);
-	ImGui::Begin("hello");
-	ImGui::Text("Hello, World!");
-	ImGui::Text(std::format("{}", ImGui::GetIO().Framerate).c_str());
-	ImGui::End();
+    ImGui::SetNextWindowSize(ImVec2(300, 100), ImGuiCond_FirstUseEver);
+    ImGui::Begin("hello");
+    ImGui::Text("Hello, World!");
+    ImGui::Text(std::format("{}", ImGui::GetIO().Framerate).c_str());
+    ImGui::End();
 }
+
 void gui::do_render()
 {
-	static std::once_flag flag;
-	std::call_once(flag, []() {
-		sdk::attach();
-		ImGui::CreateContext();
-		ImGui_ImplWin32_Init(dx_hook::Hk11::GetHwnd());
-		ImGui_ImplDX11_Init(dx_hook::Hk11::GetDevice(), dx_hook::Hk11::GetContext());
+    static std::once_flag flag;
+    std::call_once(flag, []() {
+        sdk::attach();
+        ImGui::CreateContext();
+        ImGui_ImplWin32_Init(dx_hook::Hk11::GetHwnd());
+        ImGui_ImplDX11_Init(dx_hook::Hk11::GetDevice(), dx_hook::Hk11::GetContext());
 
-		auto& io = ImGui::GetIO();
-		io.Fonts->AddFontDefault();
+        auto& io = ImGui::GetIO();
+        io.Fonts->AddFontDefault();
 
-		dx_hook::Hk11::SetWndProc([](const HWND hWnd, const UINT msg, const WPARAM wParam, const LPARAM lParam) -> char {
-			POINT mPos;
-			GetCursorPos(&mPos);
-			ScreenToClient(dx_hook::Hk11::GetHwnd(), &mPos);
-			ImGui::GetIO().MousePos.x = static_cast<float>(mPos.x);
-			ImGui::GetIO().MousePos.y = static_cast<float>(mPos.y);
-			ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam);
+        dx_hook::Hk11::SetWndProc([](const HWND hWnd, const UINT msg, const WPARAM wParam, const LPARAM lParam) -> char {
+            POINT mPos;
+            GetCursorPos(&mPos);
+            ScreenToClient(dx_hook::Hk11::GetHwnd(), &mPos);
+            ImGui::GetIO().MousePos.x = static_cast<float>(mPos.x);
+            ImGui::GetIO().MousePos.y = static_cast<float>(mPos.y);
+            ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam);
 
+            switch (msg) {
+            case WM_KEYDOWN:
+                if (wParam == VK_DELETE) {
+                    gui::show = !gui::show;
+                }
+                break;
+            case WM_KEYUP: break;
+            case WM_CLOSE:
+                const auto result = MessageBox(nullptr, L"你确定要退出游戏吗?", L"Confirmation", MB_YESNO | MB_ICONQUESTION);
+                if (result == IDYES) exit(0);
+                if (result == IDNO) return 2;
+                break;
+            }
 
-			switch (msg) {
-			case WM_KEYDOWN:
-				if (wParam == VK_DELETE) {
-					gui::show = !gui::show;
-				}
-				break;
-			case WM_KEYUP: break;
-			case WM_CLOSE: const auto result = MessageBox(nullptr, L"你确定要退出游戏吗?", L"Confirmation", MB_YESNO | MB_ICONQUESTION);
+            return !gui::show;
+            });
+        });
 
-				if (result == IDYES) exit(0);
-				if (result == IDNO) return 2;
-				break;
-			}
+    ImGui_ImplDX11_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
 
+    if (gui::show)
+    {
+		//强制显示这一块
+        ImGui::GetIO().MouseDrawCursor = true;
 
-			return !gui::show;
-			});
-		});
+        render_gui_internal();
+    }
+    else {
+        ImGui::GetIO().MouseDrawCursor = false;
+    }
 
-	ImGui_ImplDX11_NewFrame();
-	ImGui_ImplWin32_NewFrame();
-	ImGui::NewFrame();
-	if (gui::show)
-	{
-
-		render_gui_internal();
-	}
-
-	ImGui::EndFrame();
-	ImGui::Render();
-	dx_hook::Hk11::GetContext()->OMSetRenderTargets(1, dx_hook::Hk11::GetTargetView(), nullptr);
-	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+    ImGui::EndFrame();
+    ImGui::Render();
+    dx_hook::Hk11::GetContext()->OMSetRenderTargets(1, dx_hook::Hk11::GetTargetView(), nullptr);
+    ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 }
